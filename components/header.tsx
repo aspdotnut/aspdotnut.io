0fd453ce5f92728ccfb6e42a0d41fd7c460b6967
aspdotnut <nut@aspdotnut.io>
2025-12-03 03:15:46 +0100

    i hate optimization but we doin it anyways

--- components/header.tsx
+++ components/header.tsx
@@ -29,13 +29,13 @@ export default function Header(){
                     </div>
                 </div>
             </header>
-            <div className={`md:hidden block ${isActive ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} fixed top-16 left-2 z-20 transition ease-in-out duration-300 flex flex-col text-base`}>
-                    <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit"} onClick={()=>{setIsActive(!isActive)}} href={"/"}>HOME</Link>
-                    {/*<Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit"} onClick={()=>{setIsActive(!isActive)}} href={"https://blog.aspdotnut.io"} rel="noopener noreferrer">BLOG</Link>*/}
-                    <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit"} onClick={()=>{setIsActive(!isActive)}} href={"/about"}>ABOUT</Link>
-                    <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit"} onClick={()=>{setIsActive(!isActive)}} href={"/socials"}>SOCIALS</Link>
+            <div className={`md:hidden ${isActive ? "opacity-100" : "opacity-0"} pointer-events-none fixed top-16 left-2 z-20 transition ease-in-out duration-300 flex flex-col text-base`}>
+                    <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"/"}>HOME</Link>
+                    {/*<Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"https://blog.aspdotnut.io"} rel="noopener noreferrer">BLOG</Link>*/}
+                    <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"/about"}>ABOUT</Link>
+                    <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"/socials"}>SOCIALS</Link>
             </div>
-            <button className={`block ${isActive ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} bg-darkenedBlur absolute h-full w-full backdrop-blur-sm z-10 transition ease-in-out duration-300 flex flex-col text-base`}
+            <button className={`block ${isActive ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} bg-darkenedBlur absolute h-full w-full backdrop-blur-sm z-10 transition ease-in-out duration-300`}
                     onClick={()=>{
                         setIsActive(!isActive)
                     }}
