f05a03f3bd2cbad5b4f14cd79354ec23a019a69e
aspdotnut <nut@aspdotnut.io>
2025-12-03 03:32:00 +0100

    my code's appearance will be my 13th reason istg

--- components/header.tsx
+++ components/header.tsx
@@ -6,6 +6,11 @@ export default function Header(){
     const [isActive, setIsActive] = useState(false);
     return (
         <>
+            <button className={`block ${isActive ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} bg-darkenedBlur absolute h-full w-full backdrop-blur-sm z-10 transition ease-in-out duration-300`}
+                    onClick={()=>{
+                        setIsActive(!isActive)
+                    }}
+            />
             <header className={"sticky top-0 z-10"}>
                 <div className={"flex justify-between md:justify-normal gap-10 bg-special p-2 text-xl"}>
                     <Link className={"text-specialText"} href={"/"}>ASPDOTNUT</Link>
@@ -14,6 +19,12 @@ export default function Header(){
                         {/*<Link className={"hover:text-specialTextHover transition ease-in-out text-specialText"} href={"https://blog.aspdotnut.io"} rel="noopener noreferrer">BLOG</Link>*/}
                         <Link className={"hover:text-specialTextHover transition ease-in-out text-specialText"} href={"/about"}>ABOUT</Link>
                         <Link className={"hover:text-specialTextHover transition ease-in-out text-specialText"} href={"/socials"}>SOCIALS</Link>
+                    </div>
+                    <div className={`md:hidden ${isActive ? "opacity-100" : "opacity-0"} pointer-events-none fixed top-16 left-2 z-20 transition ease-in-out duration-300 flex flex-col text-base`}>
+                        <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"/"}>HOME</Link>
+                        {/*<Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"https://blog.aspdotnut.io"} rel="noopener noreferrer">BLOG</Link>*/}
+                        <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"/about"}>ABOUT</Link>
+                        <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"/socials"}>SOCIALS</Link>
                     </div>
                     <div className={"md:hidden h-[28px] flex items-center justify-center"}>
                         <button
@@ -29,17 +40,6 @@ export default function Header(){
                     </div>
                 </div>
             </header>
-            <div className={`md:hidden ${isActive ? "opacity-100" : "opacity-0"} pointer-events-none fixed top-16 left-2 z-20 transition ease-in-out duration-300 flex flex-col text-base`}>
-                    <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"/"}>HOME</Link>
-                    {/*<Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"https://blog.aspdotnut.io"} rel="noopener noreferrer">BLOG</Link>*/}
-                    <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"/about"}>ABOUT</Link>
-                    <Link className={"hover:text-specialTextHover transition ease-in-out text-6xl text-special w-fit pointer-events-auto"} onClick={()=>{setIsActive(!isActive)}} href={"/socials"}>SOCIALS</Link>
-            </div>
-            <button className={`block ${isActive ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"} bg-darkenedBlur absolute h-full w-full backdrop-blur-sm z-10 transition ease-in-out duration-300`}
-                    onClick={()=>{
-                        setIsActive(!isActive)
-                    }}
-            />
         </>
     );
 }
